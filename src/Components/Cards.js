7cd88c7f92fbc645213e0d6bf620d20c825d4b79
Marlene Guzman <guzmanmarlene@yahoo.com>
2021-11-29 22:34:12 -0800

    structuring data on the Cards component

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,19 +1,25 @@
 import React from 'react'
-
+import styles from './Cards.module.scss'
 const Cards = ({results}) => {
 
   let display;
 console.log(results)
   if( results ){
     display = results.map( x => {
-      let {id, name, image} = x
-      return( <div key={id} className="col-4">
+      let {id, name, image, location, status} = x
+      return( <div key={id} className="col-4 position-relative">
         <div className="">
           <img src={image} alt="" className="img-fluid"/>
           <div className="content">
-            <div className="">{name}</div>
+            <div className="fs-4 fw-bold mb-4">{name}</div>
+            <div className="">
+              <div className="fs-6">Last Known Location: </div>
+              <div className="fs-5">{location.name}</div>
+    
+            </div>
           </div>
         </div>
+        <span class="position-absolute badge bg-danger">{status}</span>
       </div> )
     })
   } else{
