d228ccd3cccf588ad33f876813525848c9b6c2e4
Marlene Guzman <guzmanmarlene@yahoo.com>
2021-11-29 20:59:50 -0800

    adding rick and morty. saving just in case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import "bootstrap/dist/css/bootstrap.min.css" 
+import "bootstrap/dist/js/bootstrap" 
 function App() {
   return (
     <div className="App">
-  <p>Rick And Morty</p>
-        
+      
+      <h1 className="text-center ubuntu my-4">
+      <img alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUsZ4kIPn362NpJfOnswm9zIjr1aeAfNWoMT4qJUusnz8R3nZN4KwwlB5InGUlHhmCU9s&usqp=CAU"/>
+
+      </h1>
+      
+  
     </div>
   );
 }
