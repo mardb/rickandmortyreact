367f1762d6cb4480254e1b7c2bd67b36e470a4c0
Marlene Guzman <guzmanmarlene@yahoo.com>
2021-11-30 18:51:59 -0800

    pagination complete

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -2,21 +2,21 @@ import React from 'react'
 import ReactPaginate from 'react-paginate';
 
 const Pagination = ({info, pageNumber, setPageNumber }) => {
-// let next = () => {
-//   setPageNumber(x =>  x + 1);
-// };
+  // console.log(info.pages)
 
-// let prev = () => {
-//   if(pageNumber === 1) return;
-//   setPageNumber(x =>  x - 1);
-// };
-
-  return <ReactPaginate pageCount={info.pages}/>
-    // <div className="container d-flex justify-content-center gap-5 my-5">
-    //     <button onClick={prev} className="btn btn-primary">Prev</button>
-    //     <button onClick={next} className="btn btn-primary">Next</button>
-    // </div>
-  
+  return <ReactPaginate 
+  activeClassName="active"
+  breakLabel="..."
+  className="pagination justify-content-center gap-4 my-4" 
+  forcePage={pageNumber === 1? 0 : pageNumber - 1}
+  nextLabel="Next" 
+  nextClassName="btn btn-success "
+  pageClassName="page-item"
+  pageLinkClassName="page-link"
+  previousLabel="Prev"
+  previousClassName="btn btn-success"
+  onPageChange={(data) => { setPageNumber(data.selected + 1)}}
+  pageCount={info?.pages}/>
 }
 
 export default Pagination
