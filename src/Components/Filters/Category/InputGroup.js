e41a099de670ce7509eb3fbe0e41a02b469a425e
Marlene Guzman <guzmanmarlene@yahoo.com>
2021-12-02 21:16:16 -0800

    adding a location page

---
+++ src/Components/Filters/Category/InputGroup.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const InputGroup = ({ name, setID, total}) => {
+  return (
+    <div className="input-group mb-3">
+      <select
+      onChange={(e) => setID(e.target.value)}
+      className="form-select"
+      id={name}
+      >
+        <option value="1" selected> Location...</option>
+
+        {[...Array(total).keys()].map((x) => {
+          return(
+            <option value={x + 1}>
+              {name} - {x + 1}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default InputGroup
--- src/Components/Pages/Locations.js
+++ src/Components/Pages/Locations.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Cards from '../Cards';
+import InputGroup from '../Filters/Category/InputGroup'; 
 
 const Location = () => {
+
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([])
+  let {name, type, dimension} = info;
+  console.log(info)
+  let api = `https://rickandmortyapi.com/api/location/${id}`
+
+  useEffect(()=> {
+    //IIFE
+   (async function() {
+      let data = await fetch(api).then((res)=> res.json())
+      setInfo(data);
+console.log(data)
+
+      let a = await  Promise.all(
+        data.residents.map((x) => {
+        return fetch(x).then((res) => res.json());
+        })
+      )
+      setResults(a);
+   })();
+  
+  }, [api])
   return (
-    <div>
-      LOCATIONS
+  <div className="container">
+  <div className="row mb-3">
+    <h1 className="text-center mb-3">
+      Location: {" "}
+      <span className="text-primary">{name === "" ? "Unknown" : name}</span>
+    </h1>
+    <h5 className="text-center">
+      Dimension : {dimension === "" ? "Unknown" : dimension}
+    </h5>
+    <h6 className="text-center">
+      Type: {type === "" ? "Unknown" : type}
+    </h6>
+  </div>
+  <div className="row">
+    <div className="col-lg-3 col-12 mb-4">
+      <h4 className="text-center mb-4"> Pick Location </h4>
+      <InputGroup setID={setID} name="Location" total={126}/>
     </div>
-  )
+    <div className="col-lg-8 col-12">
+      <div className="row">
+      <Cards page="/location" results={results}/>
+      </div>
+     
+    </div>
+  </div>
+  </div>
+    )
+  
+  
 }
 
 export default Location
