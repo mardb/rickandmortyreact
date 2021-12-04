4dcb6b57754458193c4bcd25575d89b0df524683
Marlene Guzman <guzmanmarlene@yahoo.com>
2021-12-04 00:17:34 -0800

    safety commit wip

--- src/Components/Cards/CardNotes.js
+++ src/Components/Cards/CardNotes.js
@@ -16,6 +16,45 @@ const CardNotes = () => {
       console.log(data);
     })();
   }, [formApi]);
+  // ----------------**FETCH**------------------------------
+
+//----------------****POST REQUEST****----------------------
+const jsonPost =()=> {
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+//PUT request
+const editPost = () => {
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }).then(res => console.log(res)),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+}
+
 
   return (
     <div className="mb-3 container">
